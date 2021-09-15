af2ca2df14a99a7acc5faaa3d57355c6d5831b98
Josip-github <josipcota1@gmail.com>
2021-09-15 05:52:11 +0200

    Mapping class Course

---
+++ Gym/src/main/java/hr/edunova/gym/model/Course.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.gym.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Vlasnik
+ */
+@Entity
+public class Course extends Object {
+    
+    private String name;
+    
+    private BigDecimal price;
+    
+    private int duration;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    
+}
