7c3638893dc84c86e7378698120d38c1b86c99f5
Josip-github <josipcota1@gmail.com>
2021-09-15 05:48:20 +0200

    Mapping class Person Classes Employee and Member will extend this class

---
+++ Gym/src/main/java/hr/edunova/gym/model/Person.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.gym.model;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author Vlasnik
+ */
+@MappedSuperclass
+public class Person extends Object {
+    
+    private String firstname;
+    
+    private String lastname;
+    
+   @Column(columnDefinition = "char(11)")
+    private String oib;
+    
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
