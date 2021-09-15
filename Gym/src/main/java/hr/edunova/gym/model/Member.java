7bcfe2d5d925b6fffa5de40e27f49e6455b9237c
Josip-github <josipcota1@gmail.com>
2021-09-15 05:49:30 +0200

    Mapping class Member

---
+++ Gym/src/main/java/hr/edunova/gym/model/Member.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.gym.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Vlasnik
+ */
+@Entity
+public class Member extends Person {
+    
+}
