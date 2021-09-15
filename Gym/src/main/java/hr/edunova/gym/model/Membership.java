cfa250d1b875909a603ac23b9d592b88c4d063d6
Josip-github <josipcota1@gmail.com>
2021-09-15 05:54:12 +0200

    Mapping class Membership

---
+++ Gym/src/main/java/hr/edunova/gym/model/Membership.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.gym.model;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Vlasnik
+ */
+@Entity
+public class Membership extends Object {
+    
+    @ManyToOne
+    private Course course;
+    
+    @ManyToOne
+    private Member member;
+    
+    private Date dateOfBegin;
+    
+    private Boolean payment;
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Date getDateOfBegin() {
+        return dateOfBegin;
+    }
+
+    public void setDateOfBegin(Date dateOfBegin) {
+        this.dateOfBegin = dateOfBegin;
+    }
+
+    public Boolean getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Boolean payment) {
+        this.payment = payment;
+    }
+    
+    
+}
