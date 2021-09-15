570488116e67df151cb7cd77b90a94b119f1e7b9
Josip-github <josipcota1@gmail.com>
2021-09-15 05:50:29 +0200

    Mapping class Employee

---
+++ Gym/src/main/java/hr/edunova/gym/model/Employee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.gym.model;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Vlasnik
+ */
+@Entity
+public class Employee extends Person {
+    
+    private String iban;
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+    
+    
+}
