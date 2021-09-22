c92540c717571d250cde8d7511aafc92e7a945de
Josip-github <josipcota1@gmail.com>
2021-09-22 05:58:23 +0200

    Creating new class Application In this class I have constant field which is the title of app, later on I can always use it

---
+++ Gym/src/main/java/hr/edunova/gym/util/Application.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.edunova.gym.util;
+
+/**
+ *
+ * @author Vlasnik
+ */
+public class Application {
+    
+    public static final String TITLE_APP = "Gym App";
+}
